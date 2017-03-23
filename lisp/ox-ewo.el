25964098ba4413d263c588febf7fcbb5c99ee9f6
Eric Würbel <eric.wurbel@univ-amu.fr>
2017-03-23 16:31:39 +0100

    Added GPL License and copyright declaration.

--- lisp/ox-ewo.el
+++ lisp/ox-ewo.el
@@ -1,3 +1,19 @@
+;; Copyright 2017 Éric Würbel
+
+;; This file is part of EWO.
+
+;; EWO is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; EWO is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with EWO.  If not, see <http://www.gnu.org/licenses/>.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Easy Website with Org (EWO) project
